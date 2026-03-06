07d66aac46d39ef50c4ded966a7a3ba8904e9263
iofahmawi <mo.fahmawi@gmail.com>
2026-03-06 05:31:47 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'deyon-v13';
+const CACHE_NAME = 'deyon-v14';
 const ASSETS = [
     './',
     './index.html',
@@ -27,3 +27,4 @@ self.addEventListener('fetch', e => {
         caches.match(e.request).then(cached => cached || fetch(e.request))
     );
 });
+
